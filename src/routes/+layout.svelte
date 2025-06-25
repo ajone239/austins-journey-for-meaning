b42d703e437fbaab22f47410275223324b6810ef
Austin Jones <ajones53.aj@gmail.com>
2025-06-25 00:01:49 -0400

    reasonable defaults

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,13 @@
 <script lang="ts">
 	import Header from '$lib/components/Header.svelte';
 	import '$lib/styles/styles.scss';
+	import type { LayoutProps } from './$types';
+
+	let { data, children }: LayoutProps = $props();
 </script>
 
 <Header />
 
 <main>
-	<slot />
+	{@render children()}
 </main>
-
-<footer>Hello, I'm the footer.</footer>
